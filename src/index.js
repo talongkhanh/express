98cfccfd251d5c8ec45c17c94f78e0e98e962784
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-12 10:56:01 +0700

    Template engine Handlebars

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 8000;
+
+app.use(morgan('combined'));
+
+app.engine('hbs', exphbs({
+	extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+	res.render('home')
+});
+
+app.listen(port, () => {
+	console.log(`Server listening at http://localhost:${port}`)
+});
