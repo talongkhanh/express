f318651d365a8710c259fea62c91c51bd68dd57f
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-08-12 19:29:08 +0700

    static files and Scss

--- src/index.js
+++ src/index.js
@@ -7,18 +7,24 @@ const app = express();
 const port = 8000;
 
 app.use(morgan('combined'));
-
+app.use(express.static(path.join(__dirname, 'public')));
+// view engine
 app.engine('hbs', exphbs({
 	extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// 
 
 app.get('/', (req, res) => {
 	res.render('home')
 });
 
+app.get('/about', (req, res) => {
+	res.render('about')
+});
+// 
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`)
 });
